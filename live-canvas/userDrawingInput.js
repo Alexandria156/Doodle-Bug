83515ad54bf3759d3ba379aee8d46f294f0c9161
Alexandria 156 <asy2678@rit.edu>
2025-11-01 17:34:25 -0400

    Added a color picker

--- live-canvas/userDrawingInput.js
+++ live-canvas/userDrawingInput.js
@@ -6,25 +6,25 @@ const context = userCanvas.getContext("2d");
 const colorPicker = document.getElementById("colorPicker");
 
 let activeDrawing = false;
-let brushSize = 15;
+let brushSize = 10;
 let brushColor = colorPicker.value;
 
 console.log("Canvas get:", userCanvas);
 console.log("Context get:", context);
 
-function startDrawing(mouse){
+function startDrawing(mouse) {
     console.log("Mouse down")
     activeDrawing = true;
     currentlyDrawing(mouse);
 }
 
-function currentlyDrawing(mouse){
+function currentlyDrawing(mouse) {
     console.log("Mouse moving")
     context.lineWidth = brushSize;
     context.lineCap = "round";
-    context.fillStyle = brushColor;
-    if(activeDrawing == true)
-    {
+    //context.fillStyle = brushColor;
+    context.strokeStyle = brushColor;
+    if (activeDrawing == true) {
         console.log("Drawing!")
         context.lineTo(mouse.offsetX, mouse.offsetY);
         context.stroke();
@@ -33,22 +33,23 @@ function currentlyDrawing(mouse){
 
     }
 }
-function endDraw()
-{
+function endDraw() {
     console.log("Mouse out or up")
     activeDrawing = false;
     context.beginPath();
 }
 
-function changeColor(change)
-{
+function changeColor(change) {
     brushColor = change.target.value;
-    console.log("Changed color ", value)
+}
+function updateColor(change) {
+    
 }
 
 userCanvas.addEventListener("mousedown", startDrawing);
 userCanvas.addEventListener("mouseout", endDraw);
-userCanvas.addEventListener("mouseup",endDraw);
-userCanvas.addEventListener("mousemove",currentlyDrawing);
+userCanvas.addEventListener("mouseup", endDraw);
+userCanvas.addEventListener("mousemove", currentlyDrawing);
 colorPicker.addEventListener("input", changeColor);
+//colorPicker.addEventListener("change", updateColor);
 
