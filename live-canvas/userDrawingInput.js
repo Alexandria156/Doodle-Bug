f8281e24a3e8340474ef504a0342f170e67ec2a1
Alexandria 156 <asy2678@rit.edu>
2025-11-01 15:50:51 -0400

    Drawinf finally works :)

--- live-canvas/userDrawingInput.js
+++ live-canvas/userDrawingInput.js
@@ -22,29 +22,62 @@ let activeDrawing = false;
 console.log("Canvas get:", userCanvas);
 console.log("Context get:", context);
 
-userCanvas.onmousedown = function () {
+function startDrawing(mouse){
+    console.log("Mouse down")
+    activeDrawing = true;
+    currentlyDrawing(mouse);
+}
+
+function currentlyDrawing(mouse){
+    console.log("Mouse moving")
+    context.lineWidth = 20;
+    context.lineCap = "butt";
+    context.fillStyle = "red";
+    if(activeDrawing == true)
+    {
+        console.log("Drawing!")
+        context.lineTo(mouse.offsetX, mouse.offsetY);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(mouse.offsetX, mouse.offsetY);
+
+    }
+}
+function endDraw()
+{
+    console.log("Mouse out or up")
+    activeDrawing = false;
+    context.beginPath();
+}
+
+userCanvas.addEventListener("mousedown", startDrawing);
+userCanvas.addEventListener("mouseout", endDraw);
+userCanvas.addEventListener("mouseup",endDraw);
+userCanvas.addEventListener("mousemove",currentlyDrawing);
+/*
+userCanvas.onmousedown = function (mouse) {
     console.log("Mouse down");
     activeDrawing = true;
 
+
 }
-userCanvas.onmousemove = function () {
+userCanvas.onmousemove = function (mouse) {
     if (activeDrawing = true) {
         console.log("Mouse moving");
         context.fillStyle = "red";
         context.lineWidth = 50;
         context.beginPath();
-
-        context.moveTo(100, 40);
-        context.lineTo(300, 87);
-
+        //context.moveTo(100, 40);
+        //context.lineTo(12, 23);
+        
         context.stroke();
     }
 }
-userCanvas.onmouseout = function () {
+userCanvas.onmouseout = function (mouse) {
     console.log("No drawing; mouse off")
     activeDrawing = false;
 }
-userCanvas.onmouseup = function () {
+userCanvas.onmouseup = function (mouse) {
     console.log("No drawing; mouse up")
     activeDrawing = false;
-}
+}*/
