e31008648256494d729ad56a8d783c273b35129b
Alexandria 156 <asy2678@rit.edu>
2025-11-02 01:34:06 -0400

    Trying to implement undo and redo

--- live-canvas/userDrawingInput.js
+++ live-canvas/userDrawingInput.js
@@ -2,10 +2,19 @@
 console.log("JS is loaded!");
 
 const userCanvas = document.getElementById("sketchCanvas");
+
 const context = userCanvas.getContext("2d");
 const colorPicker = document.getElementById("colorPicker");
-const sizeChange = document.getElementById("brushSize")
-const wipeCanvas = document.getElementById("clear")
+const sizeChange = document.getElementById("brushSize");
+const undoStroke = document.getElementById("undo");
+const redoStroke = document.getElementById("redo");
+const wipeCanvas = document.getElementById("clear");
+
+const startGame = document.getElementById("startGame");
+
+var canvasHistory = new Array();
+var canvasPushes = -1;
+
 let activeDrawing = false;
 let brushSize = sizeChange.value;
 let brushColor = colorPicker.value;
@@ -23,7 +32,6 @@ function currentlyDrawing(mouse) {
     console.log("Mouse moving")
     context.lineWidth = brushSize;
     context.lineCap = "round";
-    //context.fillStyle = brushColor;
     context.strokeStyle = brushColor;
     if (activeDrawing == true) {
         console.log("Drawing!")
@@ -50,9 +58,38 @@ function changeSize(brush){
     currentlyDrawing(brush);
 }
 
+function currentCanvas(){
+    console.log("Stored current canvas");
+    canvasPushes++;
+    if(canvasPushes < canvasHistory)
+    {
+        canvasHistory.length = canvasPushes;
+    }
+    canvasHistory.push(userCanvas.toDataURL());
+}
+
 function clearCanvas(){
     console.log("Cleared canvas");
     context.clearRect(0,0, userCanvas.width, userCanvas.height);
+}
+
+function undoWork(){
+    if(canvasPushes > 0)
+    {
+        canvasPushes--;
+        var canvasSnap = new Image();
+        canvasSnap.src=canvasHistory[canvasPushes];
+        canvasSnap.onload=function() {context.drawImage(canvasSnap, 0, 0);}
+    }
+}
+function redoWork(){
+    if(canvasPushes < canvasHistory.length-1)
+    {
+        canvasPushes++;
+        var canvasSnap = new Image();
+        canvasSnap.src = canvasHistory[canvasPushes];
+        canvasSnap.onload = function() {context.drawImage(canvasSnap, 0, 0);}
+    }
 }
 
 userCanvas.addEventListener("mousedown", startDrawing);
@@ -62,4 +99,5 @@ userCanvas.addEventListener("mousemove", currentlyDrawing);
 colorPicker.addEventListener("input", changeColor);
 sizeChange.addEventListener("input", changeSize);
 wipeCanvas.addEventListener("click", clearCanvas);
-
+undoStroke.addEventListener("click", undoWork);
+redoStroke.addEventListener("click",redoWork);
