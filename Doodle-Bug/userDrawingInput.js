8457daa47f3f621c8568b44716e339a1e42b5af7
Alexandria 156 <asy2678@rit.edu>
2025-11-02 01:36:12 -0500

    Added a scondry page to host our drawing game

--- Doodle-Bug/userDrawingInput.js
+++ Doodle-Bug/userDrawingInput.js
@@ -58,6 +58,10 @@ function clearCanvas(){
     context.clearRect(0,0, userCanvas.width, userCanvas.height);
 }
 
+function switchToGame(){
+    console.log("Switching to game");
+    window.location.href = "main.html";
+}
 
 userCanvas.addEventListener("mousedown", startDrawing);
 userCanvas.addEventListener("mouseout", endDraw);
@@ -66,3 +70,4 @@ userCanvas.addEventListener("mousemove", currentlyDrawing);
 colorPicker.addEventListener("input", changeColor);
 sizeChange.addEventListener("input", changeSize);
 wipeCanvas.addEventListener("click", clearCanvas);
+startGame.addEventListener("click", switchToGame);
